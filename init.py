c03f791602b68407b5c7f07997f678b4962f88d9
zafiro avila <avilazafiro390@gmail.com>
2025-09-24 16:06:50 -0300

    creacion de botones y meora de interfas de formulario

--- init.py
+++ init.py
@@ -1,23 +1,30 @@
 # Importamos Flet
 import flet as ft
-
+from fpdf import FPDF
 # Creamos una clase para el formulario
 class FormularioApp:
     def __init__(self):
         # Campos de texto para que el usuario ingrese datos
+        # Campos de texto para que el usuario ingrese datos
         self.nombre = ft.TextField(label="Nombre", width=300)
         self.correo = ft.TextField(label="Correo", width=300)
-        
+        self.edad = ft.TextField(label="Edad", width=300)
+        self.sangre = ft.TextField(label="Tipo de sangre", width=300)
+        self.dni = ft.TextField(label="DNI", width=300)
+        self.link_descarga = ft.Text() 
         # Texto donde se mostrará el resultado
         self.resultado = ft.Text(value="", size=16)
 
     # Función que se ejecuta cuando se presiona el botón "Enviar"
-    def enviar_datos(self, e):
-        # Tomamos los valores de los campos y los mostramos en self.resultado
-        self.resultado.value = f"Nombre: {self.nombre.value}\nCorreo: {self.correo.value}"
-        # Actualizamos la página para que se vea el cambio
+    def enviar_datos(self, e):     
+        self.resultado.value = (
+        f"Nombre: {self.nombre.value}\n"
+        f"Correo: {self.correo.value}\n"
+        f"Edad: {self.edad.value}\n"
+        f"Tipo de sangre: {self.sangre.value}\n"
+        f"DNI: {self.dni.value}"
+    )
         self.page.update()
-
     # Función principal que crea la interfaz
     def main(self, page: ft.Page):
         self.page = page  # Guardamos la referencia a la página
@@ -25,16 +32,21 @@ class FormularioApp:
         self.page.bgcolor = "white"  # Fondo blanco
         self.page.vertical_alignment = ft.MainAxisAlignment.CENTER
 
-        # Botón que ejecuta enviar_datos cuando se hace clic
-        boton = ft.ElevatedButton(text="Enviar", on_click=self.enviar_datos)
+        # Botones guardados en self
+        self.boton_enviar = ft.ElevatedButton(text="Enviar", on_click=self.enviar_datos)
+        self.boton_pdf = ft.ElevatedButton(text="Generar PDF", on_click=self.generar_pdf)
 
         # Agregamos todos los elementos a la página en una columna
         self.page.add(
             ft.Column(
-                controls=[
+                  controls=[
                     self.nombre,
                     self.correo,
-                    boton,
+                    self.edad,
+                    self.sangre,
+                    self.dni,
+                    self.boton_enviar,
+                    self.boton_pdf,
                     self.resultado
                 ],
                 alignment=ft.MainAxisAlignment.CENTER,
